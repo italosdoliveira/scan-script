45da8580d6c5279922ff60c28875a3a08ec7fb47
italoooliveira <italo.oli12@gmail.com>
2024-03-12 22:00:24 -0300

    Erro leitura subfinder

--- enumerate.sh
+++ enumerate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Definindo cores
+vermelho='\033[0;31m'
+verde='\033[0;32m'
+azul='\033[0;34m'
+sem_cor='\033[0m'
 # Função para exibir o banner
 exibir_banner() {
     echo -e "\e[1;34m"
@@ -16,63 +21,92 @@ exibir_banner() {
     echo "==============================================================="
     echo "                 Bem vindo a WSS Pentest Tool                  "
     echo "==============================================================="
-    echo "   Script desenvolvido por Bonde da WSS                        "
+    echo "   Script desenvolvido por Luciano V, Italo O, Carol G         "
     echo "==============================================================="
+}
+
+testconnectivity() {
+	if nc -zw1 google.com 443 2>/dev/null; then
+		echo -e "Connection: ${bgreen}OK${reset}"
+	else
+		echo -e "${vermelho}[!] Por favor verifique a sua conexão, tente novamente...${reset}"
+		exit 1
+	fi
+}
+
+#Função para montar a estrutura de diretórios
+monta_diretorios() {
+    mkdir -p subdominios temp network/scans_ip network/directories osint/data_leaks osint/google_dork vuls/info vuls/low vuls/medium vuls/hard vuls/critical
 }
 
 # Chamar a função para exibir o banner
 exibir_banner
 
+##testconnectivity
+
+#Chamar a função para montar a estrutura de diretórios
+monta_diretorios
+
 # Verifica se o subfinder está instalado
 if ! command -v subfinder &> /dev/null; then
-    echo "Subfinder não está instalado. Por favor, instale-o primeiro."
+    echo "${vermelho}[!] Subfinder não está instalado. Por favor, instale-o primeiro."
     exit 1
 fi
 
 # Solicita ao usuário a inserção da URL para enumerar subdomínios
-read -p "Insira a URL para enumerar subdomínios: " input_url
+read -p "${azul}[!] Insira a URL para enumerar subdomínios: " input_url
 
 # Mostra a mensagem "Enumerando subdomínios..."
-echo "Enumerando subdomínios para $input_url"
+echo -e "${azul}[!] Enumerando subdomínios de $input_url"
 
 # Utiliza o subfinder para buscar os subdomínios da URL
-subdomains=$(subfinder -d "$input_url" -silent)
+subdomains=$(subfinder -d "$input_url" | assetfinder -subs-only -all -t 100)
 
 # Testa se foram encontrados subdomínios para a URL informada
 if [ -z "$subdomains" ]; then
     echo "Nenhum subdomínio encontrado para $input_url"
 else
-    echo "Subdomínios encontrados para $input_url:"
-    echo "$subdomains"
+    echo "${azul}[!] Buscando subdominios..."
+    # Grava os subdomínios no arquivo subdomains.txt
+    echo "$subdomains" >> temp/subdomains_full.txt
 fi 
 
-# Grava os subdomínios no arquivo subdomains.txt
-echo "$subdomains" >> "subdomains.txt"
-
-# Extrai os subdomínios separados
-curl -s "https://crt.sh/?q=%.$input_url&output=json" | jq -r '.[] | select(.name_value | test("^[^*]+\\.'$input_url'$")) | .name_value' | sed 's/\*\.//' > subdomains_separated.txt
-cat subdomains.txt | tr ' ' '\n' >> subdomains_separated.txt
-
 # Filtra os subdomínios duplicados
-cat subdomains_separated.txt | sort -u > subdomains_filter.txt
+cat temp/subdomains_full.txt | anew > subdominios/subdomains_full.txt
 
 # Verifica se o comando httpx está disponível
 if ! command -v httpx &> /dev/null; then
-    echo "httpx não está instalado. Por favor, instale-o primeiro."
+    echo "${vermelho}[!] Httpx não está instalado. Por favor, instale-o primeiro."
     exit 1
 fi
 
 # Verifica os subdomínios ativos e grava no arquivo subdomains_live.txt
-httpx -status-code -mc 200 < subdomains_filter.txt >> subdomains_live.txt
-
-# Extrai apenas os nomes de domínio dos URLs
-awk '{gsub("^https?://", "", $1); split($1, arr, "/"); print arr[1]}' subdomains_live.txt > filtrados.txt
+httpx -status-code -mc 200 < subdominios/subdomains_full.txt >> subdominios/subdomains_live.txt
 
 # Verifica se o comando naabu está disponível
 if ! command -v naabu &> /dev/null; then
-    echo "naabu não está instalado. Por favor, instale-o primeiro."
+    echo "${vermelho}[!] Naabu não está instalado. Por favor, instale-o primeiro."
     exit 1
 fi
 
 # Executa o scanner de portas naabu nos subdomínios ativos
-naabu -l filtrados.txt -v -o scan_ports.txt
+naabu -l subdominios/subdomains_live.txt -v -o network/scans_ip/scan_ports.txt
+
+#enumerar diretorios e arquivos
+if ! command -v gobuster
+
+while read subdominios/subdomains_live.txt; do 
+    gobuster dir -u subdomain
+done <network/directories.txt
+
+#nuclei
+if ! command -v nuclei &> /dev/null; then
+    echo "Nuclei não está instalado. Por favor, instale-o primeiro."
+    exit 1
+fi
+
+nuclei -l subdominios/subdomains_live.txt -t nuclei-templates/ -o vuls/nuclei_results.txt
+
+echo "${vermelho}[!] Detecção de vulnerabilidades concluída. Resultados gravados em 'nuclei_results.txt'."
+
+rm -rf temp
